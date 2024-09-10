b67b02222be2b6003325351a44473c09e52d67e4
lorenacruz01 <lorenacruz8201@gmail.com>
2024-09-09 22:44:01 -0300

    [application] - Início da implementação da camada application

---
+++ FreeLancerAPP/FreeLancer.Application/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,22 @@
+﻿using FreeLancer.Application.InputModels;
+using FreeLancer.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeLancer.Application.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        List<ProjectViewModel> GetAll(string query);
+        ProjectDetailsViewModel GetById(int id);
+        int Create(NewProjectInputModel inputModel);
+        void CreateComment(CreateCommentInputModel inputModel);
+        void Update(UpdateProjectInputModel inputModel);
+        void Delete(int id);
+        void Start(int id);
+        void Finish(int id);
+    }
+}
